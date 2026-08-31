2cb61cdb56d60e5063ead87fbfb2ff1e3b0c4f45
Jed Nicholson <jed.nicholson@durham.ac.uk>
2026-08-31 11:45:36 +0100

    added healthcheck endpoint

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const server = express()
 server.use(express.static('client'))
 
+
+server.get('/healthcheck', (req, res) => {
+    const data = {
+        status: 'ok',
+        uptime: process.uptime(),
+        timestamp: Date.now()
+    }
+    res.json(data)
+    res.send(200)
+})
+
 server.listen(3000, () => {
     console.log('server running on port 3000');
 })
